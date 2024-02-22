68f88e38106902c8be0f53e150f6aab4190861b1
Janett Gerrard <mikanchik@gmail.com>
2024-02-22 15:19:04 +0000

    work on stopwatch function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let gameOver = false
+let intervalID
 
-const startTimer = () => {
+const stopwatch = () => {
+  const start = Date.now();
+  intervalID = setInterval(() => {
+    // every second do a thing
+    let seconds = Math.floor((Date.now() - start)/1000)
+    displayTime(seconds)
+  }, 1000);
+}
+
+const displayTime = (elapsed) => {
+  console.log(elapsed)
 }
 
 const stopTimer = () => {
@@ -13,11 +24,12 @@ const moveSnail = (snail) => {
 
 const winCheck = (num) => {
   const currentSnail = (num > 0) ? "red" : "green"
-  console.log("checking win")
+  // console.log("checking win")
   document.querySelectorAll(".space:last-child").forEach((ele) => {
     if (ele.classList.contains("active")) {
-      console.log("winning is happening");
+      // console.log("winning is happening");
       gameOver = true;
+      clearInterval(intervalID)
       setTimeout(() => { window.alert(`${currentSnail} is the winner`) }, 100);
     }
   });
@@ -27,14 +39,13 @@ const handleKeyPress = (e) => {
   if (gameOver) {
     return
   }
-  console.log(e.code);
+  // console.log(e.code);
   const snail1 = document.querySelector("#player1-race > .active");
   const snail2 = document.querySelector("#player2-race > .active");
 
   switch (e.code) {
     case "KeyP":
       moveSnail(snail2);
-      console.log("moved")
       winCheck(1);
       break;
     case "KeyQ":
@@ -46,7 +57,7 @@ const handleKeyPress = (e) => {
 
 
 document.getElementById("start-button").addEventListener("click", () => {
-  startTimer();
+  stopwatch();
   document.addEventListener("keyup", handleKeyPress);
 })
 
